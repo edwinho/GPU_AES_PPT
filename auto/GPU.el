ef525d0d5ba6815fe1b14e8fac798b0c3c7d6da0
edwinho <edwin.z.ho@gmail.com>
2013-06-15 23:13:31 +0800

    TeX

---
+++ auto/GPU.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook "GPU"
+ (lambda ()
+    (TeX-run-style-hooks
+     "graphicx"
+     "tikz"
+     "xeCJK"
+     "fontenc"
+     "T1"
+     "times"
+     "inputenc"
+     "latin1"
+     "babel"
+     "english"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "")))
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is a presentation written in LaTeX.
